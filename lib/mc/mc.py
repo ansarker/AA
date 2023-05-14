83a06c744abf7cb3b340703f5af333c0e1d41e9b
ansarker <shahidsarker95@hotmail.com>
2023-05-14 17:55:22 +0600

    added

---
+++ lib/mc/mc.py
@@ -0,0 +1,76 @@
+#
+# MC: Mesh Creating
+#
+
+import cv2
+import numpy as np
+import triangle as tr
+
+from lib.mc import mc_utils
+from lib.interfaces import Mesh
+from utils import image_utils as im_utils
+
+
+class TriangleMeshCreator:
+    def __init__(self, interval=20, angle_constraint=20, area_constraint=200, dilated_pixel=5):
+        self.interval = interval
+        self.angle_constraint = angle_constraint
+        self.area_constraint = area_constraint
+        self.dilated_pixel = dilated_pixel
+
+    def create(self, image, mask):
+        """
+        Step1: sampling
+        Step2: triangulate
+
+        :param image:
+        :param mask:
+        :return:
+        """
+        mask = cv2.dilate(mask,
+                          kernel=np.ones((self.dilated_pixel, self.dilated_pixel), dtype=np.uint8),
+                          iterations=1)
+
+        sampling_points = mc_utils.sampling_points(image, mask, interval=self.interval)
+        vertices, faces = mc_utils.do_triangulate(sampling_points, mask, self.angle_constraint, self.area_constraint)
+
+        #
+        size = (image.shape[1], image.shape[0])
+        normalized_vertices = Mesh.normalize_vertices(vertices, size)
+        mesh = Mesh(normalized_vertices, faces + 1, texture_image=image)
+
+        return mesh
+
+
+def main_mc():
+    image = cv2.imread('./../../data/1261172_47.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    mask = cv2.imread('./../../data/1261172_47_mask.png', 0)
+    mask = mask.astype(np.uint8)
+
+    # im_utils.imshow(image)
+    # im_utils.imshow(mask)
+
+    mesh_mc_algo = TriangleMeshCreator()
+    m = mesh_mc_algo.create(image, mask)
+    triangle_mesh_image = m.get_image()
+    im_utils.imshow(triangle_mesh_image)
+
+    #
+    #
+    # vertices = vertices.astype(np.int32)
+    # faces = faces.astype(np.int32)
+    #
+    # # visualize vertices & faces
+    # vis_image = image.copy()
+    # for x, y in vertices:
+    #     cv2.circle(vis_image, (x, y), radius=3, color=(255,0,0), thickness=1)
+    #
+    # im_utils.imshow(vis_image)
+
+
+if __name__ == '__main__':
+    main_mc()
+
+
---
+++ lib/mc/mc_utils.py
@@ -0,0 +1,50 @@
+import numpy as np
+import triangle as tr
+from utils import image_utils as im_utils
+
+
+def sampling_points(image, image_mask, interval=20):
+    """
+    Step1: get boundary contour
+    Step2: sampling by interval
+
+    :param image:
+    :param image_mask:
+    :param interval
+    :return:
+    """
+
+    boundary_contour, _ = im_utils.get_contour(image_mask)
+    boundary_points = boundary_contour[::int(interval), 0, :].astype(np.int32)
+    return boundary_points
+
+
+def do_triangulate(vertices, image_mask, angle_constraint=20, area_constraint=100):
+    """
+
+    :param vertices: vertices
+    :param image_mask:
+    :param angle_constraint:
+    :param area_constraint:
+    :return:
+    """
+    segments = []
+    n_points = len(vertices)
+    for start_i, end_i in zip(
+            range(0, n_points - 1), range(1, n_points)
+    ):
+        segments += [(start_i, end_i)]
+    segments += [(n_points - 1, 0)]
+
+    # definition is here:
+    # https://rufat.be/triangle/API.html
+    output_dict = tr.triangulate(tri={
+        'vertices': vertices,
+        'segments': segments
+    }, opts='-p -q%s -D -Y -a%s' % (str(angle_constraint), str(area_constraint)))
+
+    #
+    vertices = output_dict['vertices']
+    faces = output_dict['triangles']
+
+    return vertices, faces
